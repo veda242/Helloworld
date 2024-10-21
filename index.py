78cce26880cf09536a848eb7c0795feb0819897a
vedasri <veda.gdm44@gmail.com>
2024-10-21 16:09:11 +0530

    Initial commit

--- index.py
+++ index.py
@@ -1,3 +1,3 @@
 import pandas as pd
-dataset=pd.read_csv(r"C:\Users\NavyaVeda\OneDrive\Desktop\fitness tracker\ps.csv")
-print(dataset.head(10))
+dataset=pd.read_csv(r"C:\Users\NavyaVeda\Downloads\fitness_tracker_dataset.csv (1)\fitness_tracker_dataset.csv")
+print(dataset.head(14))
