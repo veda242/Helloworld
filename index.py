5bcd970c064648413aa091b3039d41bfb3603f99
vedasri <veda.gdm44@gmail.com>
2024-10-21 16:29:35 +0530

    Initial commit

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
 import pandas as pd
 dataset=pd.read_csv(r"C:\Users\NavyaVeda\Downloads\fitness_tracker_dataset.csv (1)\fitness_tracker_dataset.csv")
-print(dataset.head(14))
+print(dataset.head(10))
+dataset.fillna(dataset.mean, inplace=True)
+i=dataset["steps","distance_km","active_minutes"]
